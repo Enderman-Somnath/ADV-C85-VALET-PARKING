33cfcf164f1c880dcc7fc31d8ab5a624b7020779
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2022-02-06 21:54:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+greencar_width = 75;
+greencar_height = 100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+greencar_x = 5;
+greencar_y = 225;
+
+function add() {
+	background_imgTag = new Image();
+	background_imgTag.onLoad = uploadBackground;
+	background_imgTag.src = background_image;
+	
+	greencar_imgTag = new Image();
+	greencar_imgTag.onLoad = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar.width, greencar.height);
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	greencar_y = greenrcar_y - 10;
+	uploadBackground();
+	uploadgreencar();
+}
+
+function down()
+{
+	greencar_y = greenrcar_y + 10;
+	uploadBackground();
+	uploadgreencar();
+}
+
+function left()
+{
+	greencar_x = greenrcar_x - 10;
+	uploadBackground();
+	uploadgreencar();
+}
+
+function right()
+{
+	greencar_y = greenrcar_y + 10;
+	uploadBackground();
+	uploadgreencar();
+}
